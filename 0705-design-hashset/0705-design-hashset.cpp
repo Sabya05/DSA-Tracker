f59d0938923340701d941458eef10876839d82e0
Sabyasachee Sahoo <sabyasachee01@gmail.com>
2026-07-27 20:33:47 +0530

    Time: 29 ms (22.96%), Space: 48.1 MB (76.31%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,29 @@
+class MyHashSet {
+public:
+    MyHashSet() {
+        
+    }
+    unordered_map<int,int>m;
+    void add(int key) {
+        m[key]=1;
+    }
+    
+    void remove(int key) {
+        
+           m.erase(key);
+       
+    }
+    
+   bool contains(int key) {
+    return m.find(key) != m.end();
+}
+
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */

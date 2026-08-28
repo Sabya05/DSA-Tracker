947faa93b0814f3c6ca1ff93da81330fc53c0598
Sabyasachee Sahoo <sabyasachee01@gmail.com>
2026-08-28 11:32:25 +0530

    Time: 0 ms (100%), Space: 8.1 MB (99.21%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+       for(int i=0;i<32;i++){
+            int d=n%2;
+            if(d==1)count++;
+            n=n/2;
+        }
+        return count;
+    }
+};
